1ca08a31717b47ec1af0b305059b80bdcdac271b
gilbox <birmangil@gmail.com>
2015-07-12 22:27:16 -0500

    :lipstick:

--- src/time-travel-plugin/Scrubber.js
+++ src/time-travel-plugin/Scrubber.js
@@ -4,8 +4,17 @@ import {elegant, sub} from 'elegant-react';
 const inc = x => x + 1;
 
 const styles = {
-  controlRange: { display: 'inline-block', lineHeight: '30px', verticalAlign: 'middle', width: 'calc(100% - 60px)' },
-  controlValue: { display: 'inline-block', lineHeight: '30px', verticalAlign: 'middle', color: 'grey', fontSize: '11px', width: '50px', paddingLeft: '5px' }
+  controlRange: { display: 'inline-block',
+                  lineHeight: '30px',
+                  verticalAlign: 'middle',
+                  width: 'calc(100% - 60px)' },
+  controlValue: { display: 'inline-block',
+                  lineHeight: '30px',
+                  verticalAlign: 'middle',
+                  color: 'grey',
+                  fontSize: '11px',
+                  width: '50px',
+                  paddingLeft: '5px' }
 }
 
 @elegant
